5d265edd2db4875902b5c21cd5038e215e6ef471
Mihir Jadhav <jadhav.mihir5@gmail.com>
2025-01-16 17:47:50 +0530

    integration doen for constrcution adn realty all pages dynamically

--- src/app/(group1)/realty/page.js
+++ src/app/(group1)/realty/page.js
@@ -1,64 +1,123 @@
-import ProjectCardOne from '@/components/ui/cards/projectCardOne'
-import SectionTitle from '@/components/ui/sectionTitle'
-import { projectsData } from '@/lib/fackData/projectsData'
-import { cardSlideAnimation, cardSlideAnimationDelay, cardSlideAnimationRight, cardSlideAnimationRightDelay } from '@/lib/utils';
+"use client"; // Add this since we're using useEffect and useState
 
-export const metadata = {
-    title: "Kalpana Struct-Con -- Product Archive",
-    description: "Kalpana Struct-Con is a next js and tailwind css website",
-};
+import { useEffect, useState } from "react";
+import ProjectCardOneRealty from "@/components/ui/cards/projectCardOneRealty";
+import SectionTitle from "@/components/ui/sectionTitle";
+import {
+  cardSlideAnimation,
+  cardSlideAnimationDelay,
+  cardSlideAnimationRight,
+  cardSlideAnimationRightDelay,
+} from "@/lib/utils";
 
 const ProjectArchive = () => {
-    return (
-        <section>
-            <div className='container-fluid '>
-                <SectionTitle sectionName={"Archive"} sectionTitle={"A Journey Through Our Past Projects"} sectionDesc={"Exploring the Tapestry of Our Design Legacy"} />
-            </div>
-            <div className='lg:pt-30 2sm:pt-20 pt-14'>
-                <div className=''>
-                    {
-                        projectsData.map(({ id, project_desc, project_img, project_name, project_year, project_type, location, link }) => {
-                            if (id % 2 === 0) {
-                                return (
-                                    <ProjectCardOne
-                                        key={id}
-                                        project_desc={project_desc}
-                                        project_img={project_img}
-                                        project_type={project_type}
-                                        location={location}
-                                        project_year={project_year}
-                                        link={link}
-                                        project_name={project_name}
-                                        order={'lg:order-1 order-0'}
-                                        position={"lg:absolute lg:right-0 lg:top-1/2 lg:-translate-y-1/2"}
-                                        imageVariants={cardSlideAnimationRight()}
-                                        cardVariants={cardSlideAnimationRightDelay()}
-                                    />
-                                )
-                            }
-                            else {
-                                return (
-                                    <ProjectCardOne
-                                        key={id}
-                                        project_desc={project_desc}
-                                        project_img={project_img}
-                                        project_type={project_type}
-                                        location={location}
-                                        project_year={project_year}
-                                        link={link}
-                                        project_name={project_name}
-                                        position={"lg:absolute lg:left-0 lg:top-1/2 lg:-translate-y-1/2"}
-                                        imageVariants={cardSlideAnimation()}
-                                        cardVariants={cardSlideAnimationDelay()}
-                                    />
-                                )
-                            }
-                        })
-                    }
-                </div>
-            </div>
-        </section>
-    )
-}
+  const [projects, setProjects] = useState([]);
 
-export default ProjectArchive
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch(
+          "https://kscplcms.cubeone.in/api/realties?populate=realtyProjects.mainImage&populate=realtyProjects.allImages"
+        );
+        const data = await response.json();
+
+        if (!data.data) {
+          throw new Error("No data found");
+        }
+
+        // Map the API response to the required format
+        const formattedProjects = data.data.map((project) => {
+          const { title, description, location, mainImage, allImages } = project.realtyProjects || {};
+
+          return {
+            id: project.id,
+            project_name: title || "No Title",
+            project_desc: description ? description.map(desc => desc.children.map(child => child.text).join(' ')).join(' ') : "No Description",
+            project_img: mainImage?.url || "",
+            project_type: "Realty", // Assuming all projects are of type "Realty"
+            location: location || "No Location",
+            project_year: "N/A", // No year provided in the API response
+            link: `/realty/${title
+              ?.toLowerCase()
+              .replace(/\s+/g, "-")
+              .replace(/[^a-z0-9-]/g, "")}`,
+          };
+        });
+
+        setProjects(formattedProjects);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <section>
+      <div className="container-fluid">
+        <SectionTitle
+          sectionName={"Archive"}
+          sectionTitle={"A Journey Through Our Past Projects"}
+          sectionDesc={"Exploring the Tapestry of Our Design Legacy"}
+        />
+      </div>
+      <div className="lg:pt-30 2sm:pt-20 pt-14">
+        <div className="">
+          {projects.map(
+            (
+              {
+                id,
+                project_desc,
+                project_img,
+                project_name,
+                project_year,
+                project_type,
+                location,
+                link,
+              },
+              index
+            ) => {
+              if (index % 2 === 0) {
+                return (
+                  <ProjectCardOneRealty
+                    key={id}
+                    project_desc={project_desc}
+                    project_img={project_img}
+                    project_type={project_type}
+                    location={location}
+                    project_year={project_year}
+                    link={link}
+                    project_name={project_name}
+                    order={"lg:order-1 order-0"}
+                    position={"lg:absolute lg:right-0 lg:top-1/2 lg:-translate-y-1/2"}
+                    imageVariants={cardSlideAnimationRight()}
+                    cardVariants={cardSlideAnimationRightDelay()}
+                  />
+                );
+              } else {
+                return (
+                  <ProjectCardOneRealty
+                    key={id}
+                    project_desc={project_desc}
+                    project_img={project_img}
+                    project_type={project_type}
+                    location={location}
+                    project_year={project_year}
+                    link={link}
+                    project_name={project_name}
+                    position={"lg:absolute lg:left-0 lg:top-1/2 lg:-translate-y-1/2"}
+                    imageVariants={cardSlideAnimation()}
+                    cardVariants={cardSlideAnimationDelay()}
+                  />
+                );
+              }
+            }
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectArchive;
--- src/components/ui/cards/projectCardOne.jsx
+++ src/components/ui/cards/projectCardOne.jsx
@@ -1,96 +1,75 @@
 "use client";
-import Image from "next/image";
+import Image from 'next/image';
 import { motion } from "framer-motion";
-import RightArrow from "@/assets/icons/rightArrow";
-import ButtonFill from "../buttons/buttonFill";
-import Link from "next/link";
-import { staticBluarDataUrl } from "@/lib/staticBluarDataUrl";
+import RightArrow from '@/assets/icons/rightArrow';
+import ButtonFill from '../buttons/buttonFill';
+import Link from 'next/link';
+import { staticBluarDataUrl } from '@/lib/staticBluarDataUrl';
 
-const ProjectCardOne = ({
-  order,
-  position,
-  project_year,
-  project_desc,
-  project_img,
-  location,
-  project_type,
-  project_name,
-  link,
-  imageVariants,
-  cardVariants,
-}) => {
-  return (
-    <div className="xl:max-w-[95%] w-full mx-auto relative overflow-hidden">
-      <div className="container">
-        <div className="flex lg:flex-row flex-col items-center lg:pb-[170px] pb-25">
-          <div className={`${order} lg:w-1/2`}>
-            <motion.div
-              className={`${position} w-full xl:max-w-[1100px] max-w-[800px] h-[500px] lg:h-[700px] relative`} // Adjusted height for consistency
-              initial="offscreen"
-              whileInView="onscreen"
-              variants={imageVariants}
-              viewport={{ once: true, amount: 0 }}
-            >
-              <Image
-                src={project_img}
-                loading="lazy"
-                placeholder="blur"
-                blurDataURL={staticBluarDataUrl}
-                alt={project_name}
-                fill // Use `fill` to make the image cover the container
-                className="object-cover" // Ensures the image covers the container while maintaining aspect ratio
-              />
-            </motion.div>
-          </div>
-          <motion.div
-            initial="offscreen"
-            whileInView="onscreen"
-            variants={cardVariants}
-            viewport={{ once: true, amount: 0 }}
-            className="lg:w-1/2 lg:mt-[187px] relative z-[1]"
-          >
-            <div className="bg-primary xl:px-16 px-8 xl:pt-[78px] pt-10 xl:pb-[58px] pb-7">
-              <h3 className="xl:text-5xl md:text-[40px] text-4xl font-extrabold leading-120 text-secondary-foreground pb-10">
-                {project_name}
-              </h3>
-              <p className="text-secondary-foreground mb-7">{project_desc}</p>
-              <ul className="mb-9">
-                <li className="flex items-center mb-[6px]">
-                  <span className="inline-block font-extrabold min-w-32 text-secondary-foreground">
-                    Year:
-                  </span>
-                  <span className="font-medium text-secondary-foreground">
-                    {project_year}
-                  </span>
-                </li>
-                <li className="flex items-center mb-[6px]">
-                  <span className="inline-block font-extrabold min-w-32 text-secondary-foreground">
-                    Category:
-                  </span>
-                  <span className="font-medium text-secondary-foreground">
-                    {project_type}
-                  </span>
-                </li>
-                <li className="flex items-center mb-[6px]">
-                  <span className="inline-block font-extrabold min-w-32 text-secondary-foreground">
-                    Location:
-                  </span>
-                  <span className="font-medium text-secondary-foreground">
-                    {location}
-                  </span>
-                </li>
-              </ul>
-              <Link href={link}>
-                <ButtonFill className="border-secondary text-primary-foreground hover:text-secondary-foreground after:left-0 after:bg-secondary">
-                  View Project <RightArrow width={"35"} height={"22"} />
-                </ButtonFill>
-              </Link>
+const ProjectCardOne = ({ order, position, project_year, project_desc, project_img, location, project_type, project_name, link, imageVariants, cardVariants }) => {
+    return (
+        <div className='xl:max-w-[95%] w-full mx-auto relative overflow-hidden'>
+            <div className='container'>
+                <div className='flex lg:flex-row flex-col items-center lg:pb-[170px] pb-25'>
+                    <div className={`${order} lg:w-1/2 w-full`}> {/* Ensure full width on small screens */}
+                        <div className={`${position} w-full xl:max-w-[1100px] max-w-[800px]`}>
+                            <motion.div
+                                initial="offscreen"
+                                whileInView="onscreen"
+                                variants={imageVariants}
+                                viewport={{ once: true, amount: 0 }}
+                            >
+                                {/* Adjust image container for small screens */}
+                                <div className="relative w-full aspect-[4/3] lg:h-[700px] h-[300px]"> {/* Responsive height */}
+                                    <Image 
+                                        src={project_img} 
+                                        loading='lazy' 
+                                        placeholder='blur' 
+                                        blurDataURL={staticBluarDataUrl} 
+                                        alt='project-img-1' 
+                                        layout="fill" 
+                                        objectFit="cover" 
+                                        className="object-cover" // Ensure the image covers the container
+                                    />
+                                </div>
+                            </motion.div>
+                        </div>
+                    </div>
+                    <motion.div
+                        initial="offscreen"
+                        whileInView="onscreen"
+                        variants={cardVariants}
+                        viewport={{ once: true, amount: 0 }}
+                        className='lg:w-1/2 lg:mt-[187px] mt-10 relative z-[1] w-full max-w-[700px]' // Constant width for text box
+                    >
+                        <div className={`bg-primary xl:px-16 px-8 xl:pt-[78px] pt-10 xl:pb-[58px] pb-7 w-full`}> {/* Ensure full width within the constant container */}
+                            <h3 className='xl:text-5xl md:text-[40px] text-4xl font-extrabold leading-120 text-secondary-foreground pb-10'>{project_name}</h3>
+                            <p className='text-secondary-foreground mb-7'>{project_desc}</p>
+                            <ul className='mb-9'>
+                                <li className='flex items-center mb-[6px]'>
+                                    <span className='inline-block font-extrabold min-w-32 text-secondary-foreground'>Year:</span>
+                                    <span className='font-medium text-secondary-foreground'>{project_year}</span>
+                                </li>
+                                <li className='flex items-center mb-[6px]'>
+                                    <span className='inline-block font-extrabold min-w-32 text-secondary-foreground'>Category:</span>
+                                    <span className='font-medium text-secondary-foreground'>{project_type}</span>
+                                </li>
+                                <li className='flex items-center mb-[6px]'>
+                                    <span className='inline-block font-extrabold min-w-32 text-secondary-foreground'>Location:</span>
+                                    <span className='font-medium text-secondary-foreground'>{location}</span>
+                                </li>
+                            </ul>
+                            <Link href={link}>
+                                <ButtonFill className={"border-secondary text-primary-foreground hover:text-secondary-foreground after:left-0 after:bg-secondary"}>
+                                    View Project <RightArrow width={'35'} height={"22"} />
+                                </ButtonFill>
+                            </Link>
+                        </div>
+                    </motion.div>
+                </div>
             </div>
-          </motion.div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProjectCardOne;
